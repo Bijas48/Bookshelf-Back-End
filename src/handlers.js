0d93fbd8fc9bf2c025079ecb5ce805fad59297c2
Bijas48 <adriankwk9m@gmail.com>
2024-05-09 11:36:22 +0700

    Memperbaiki logaritma Handler AddBook

--- src/handlers.js
+++ src/handlers.js
@@ -13,11 +13,31 @@ const addBookHandler = (request, h) => {
     reading,
   } = request.payload;
 
+  if (name === undefined) {
+    const response = h.response({
+      status: "fail",
+      message: "Gagal menambahkan buku. Mohon isi nama buku",
+    });
+    response.code(400);
+
+    return response;
+  }
+
+  if (pageCount < readPage) {
+    const response = h.response({
+      status: "fail",
+      message:
+        "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount",
+    });
+    response.code(400);
+
+    return response;
+  }
+
   const id = nanoid(16);
   const insertedAt = new Date().toISOString();
   const updatedAt = insertedAt;
   const finished = pageCount === readPage;
-
   const newBook = {
     id,
     name,
@@ -27,8 +47,8 @@ const addBookHandler = (request, h) => {
     publisher,
     pageCount,
     readPage,
-    reading,
     finished,
+    reading,
     insertedAt,
     updatedAt,
   };
@@ -36,25 +56,6 @@ const addBookHandler = (request, h) => {
   books.push(newBook);
 
   const isSuccess = books.filter((book) => book.id === id).length > 0;
-
-  if (!name) {
-    const response = h.response({
-      status: "fail",
-      message: "Gagal menambahkan buku. Mohon isi nama buku",
-    });
-    response.code(400);
-    return response;
-  }
-
-  if (readPage > pageCount) {
-    const response = h.response({
-      status: "fail",
-      message:
-        "Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount",
-    });
-    response.code(400);
-    return response;
-  }
 
   if (isSuccess) {
     const response = h.response({
@@ -65,14 +66,16 @@ const addBookHandler = (request, h) => {
       },
     });
     response.code(201);
+
     return response;
   }
 
   const response = h.response({
     status: "fail",
-    message: "Catatan gagal ditambahkan",
+    message: "Buku gagal ditambahkan",
   });
   response.code(500);
+
   return response;
 };
 
